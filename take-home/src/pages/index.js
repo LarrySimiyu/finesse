17eb5b1724a889ef33ce58ff3621a1941c71eb7e
Larry Simiyu <larry.simiyu@gmail.com>
2023-05-02 18:41:06 -0700

    simple accordian added

--- take-home/src/pages/index.js
+++ take-home/src/pages/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { AlignJustify, Search } from "react-feather";
@@ -5,6 +7,11 @@ import { AlignJustify, Search } from "react-feather";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
   const sizes = ["xs", "sm", "md", "lg", "xl", "1x", "2x", "3x"];
 
   return (
@@ -28,16 +35,17 @@ export default function Home() {
         </div>
       </div>
 
-      <div className=" w-full h-[500px] mt-20  lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className=" w-full h-[500px] bg-gray-300 mt-20  lg:mb-0 lg:grid-cols-4 lg:text-left pt-0">
         CLOTHING
       </div>
 
-      <div className=" w-full flex flex-col px-10">
+      <div className=" w-full flex flex-col px-5 py-10">
+        {/* name section */}
         <div className="flex justify-between">
           <div className="font-bold">Sandie Tie Dye Print Dress</div>
           <div className="text-gray-400">$500</div>
         </div>
-
+        {/* sizing section */}
         <div className="flex flex-col">
           <div className="text-[12px]">Size</div>
           <div className="grid grid-cols-4 grid-rows-2 gap-1">
@@ -50,6 +58,63 @@ export default function Home() {
               </div>
             ))}
           </div>
+
+          <div className="border flex justify-center items-center mt-4 h-10 border-black rounded-md">
+            <div className="flex justify-center w-1/3 "> - </div>
+            <div className="flex justify-center items-center w-1/3 border-l border-r h-full border-black font-bold">
+              1
+            </div>
+            <div className="flex justify-center w-1/3 "> +</div>
+          </div>
+
+          <div className="border flex justify-center items-center mt-4 h-10 border-black rounded-md font-bold text-white bg-black">
+            ADD TO CART
+          </div>
+          <div className="border flex justify-center items-center mt-4 h-10 border-black rounded-md font-bold text-white bg-black">
+            BUY WITH
+          </div>
+        </div>
+
+        <div className="flex flex-col mt-10">
+          <div className="font-bold">Product Details</div>
+          <div className="text-[14px] mb-2">
+            Angel on earth vibes - look otherworldly in this tie dye printed
+            dress 🦋 Having them all trying to figure out where you came from…
+            heaven, duh 😇
+          </div>
+
+          <div className="font-bold">Model</div>
+          <div className="text-[14px] mb-2">
+            Casey is 5'11" and is wearing a size S
+          </div>
+
+          <div className="font-bold">Material</div>
+          <div className="text-[14px] mb-2">95% Polyester 5% Spandex</div>
+        </div>
+
+        <div className="border rounded-md overflow-hidden">
+          <div
+            className="px-4 py-3 flex justify-between items-center cursor-pointer"
+            onClick={toggleAccordion}
+          >
+            <h3 className="text-sm text-gray-800">Shipping and Returns</h3>
+            <svg
+              className={`w-6 h-6 transition-transform transform ${
+                isOpen ? "rotate-180" : ""
+              }`}
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M16.59 8.59L12 13.17L7.41 8.59L6 10L12 16L18 10L16.59 8.59Z"
+              />
+            </svg>
+          </div>
+          {isOpen && (
+            <div className="bg-gray-100 px-4 py-3">
+              <p className="text-gray-800">WOW THIS IS COOL STUFF</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
